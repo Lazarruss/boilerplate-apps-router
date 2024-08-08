0c890ea7ce20cfb1d315b8a959d8dd9191ebd714
Matheus Sousa <matheus.sous.al@hotmail.com>
2024-08-08 18:24:20 -0300

    Keep calm and commit

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,6 @@
-const Main = () => {
-  ;<main>
+const Main = () => (
+  <main>
     <h1>React Avançado</h1>
   </main>
-}
+)
 export default Main
---
+++ src/components/Main/test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react'
+import Main from '.'
+
+describe('<Main />', () => {
+  it('should render the rending', () => {
+    render(<Main />)
+  })
+})
