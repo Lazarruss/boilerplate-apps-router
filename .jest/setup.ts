908da980ad5adf9c9064c0d9e60467ddbd4f75c7
Matheus Sousa <matheus.sous.al@hotmail.com>
2024-08-08 17:45:56 -0300

    Keep calm and commit

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,1 +1,5 @@
-import 'testing-library/jest-dom'
+// In your own jest-setup.js (or any other name)
+import '@testing-library/jest-dom'
+
+// In jest.config.js add (if you haven't already)
+setupFilesAfterEnv: ['<rootDir>/jest-setup.js']
