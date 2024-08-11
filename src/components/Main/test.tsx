3f03deb79f8693a7cc81bf9099f83d5ef390f0ca
Matheus Sousa <matheus.sous.al@hotmail.com>
2024-08-11 11:09:25 -0300

    Keep calm and commit

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,10 +1,12 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Main from '.'
 
 describe('<Main />', () => {
   it('should render the rending', () => {
     render(<Main />)
 
-    // expect(('heading', {name: /react avançado/i})).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
   })
 })
